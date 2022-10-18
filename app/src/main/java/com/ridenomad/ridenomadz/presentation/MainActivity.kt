0057ffac81003934c900e9202606f4fa2151c220
ARYAN GANGWAR <aryangangwar000@gmail.com>
2022-10-19 02:26:08 +0530

    feat:DetailScreen

--- app/src/main/java/com/ridenomad/ridenomadz/presentation/MainActivity.kt
+++ app/src/main/java/com/ridenomad/ridenomadz/presentation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ridenomad.ridenomadz.presentation.details.app
 import com.ridenomad.ridenomadz.presentation.theme.ui.RideNomadZTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("RideNomadZ")
+                    app(context = this)
                 }
             }
         }
